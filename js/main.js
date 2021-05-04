078a8b457a8a3eda54bc3c2649e416ebf4d84632
DESKTOP-ISG2R1R\bang1 <bang12a12a>
2021-05-04 23:18:19 +0700

    èds

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@
 	    dots: true,
 	    autoplayHoverPause: false,
 	    items: 1,
-	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
+	    navText : ["<span class='ion-ios-arrow-back'>Lùi</span>","<span class='ion-ios-arrow-forward'>Tiến</span>"],
 	    responsive:{
 	      0:{
 	        items:1
